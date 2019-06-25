75f3517804f5ff6a8a80786f7bc59f7c30ef14bf
pedroisbanjunio19 <pedroisbanjunio19.gmail.com>
2019-06-25 11:31:52 +0200

    v2

--- src/app/pipes/numero.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NumeroPipe } from './numero.pipe';
-
-describe('NumeroPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NumeroPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
